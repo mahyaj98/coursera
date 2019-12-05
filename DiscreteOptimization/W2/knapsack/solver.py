53158468f99022695272b91c92503083023ffaf4
mahyaj98 <jamshidianm98@gmail.com>
2019-12-05 23:46:20 +0330

    greedy smallest weight first - bug

--- DiscreteOptimization/W2/knapsack/solver.py
+++ DiscreteOptimization/W2/knapsack/solver.py
@@ -21,18 +21,18 @@ def solve_it(input_data):
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
 
-    #print(items)
+    print(items)
 
     # a trivial greedy algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
 
     # Greedy - Highest Value First
     chosen = []
-    max_value = 0
     tmp = items[0]
     cap = capacity
     val = 0
     while 1:
+        max_value = 0
         for item in items:
             if item.value > max_value and item.index not in chosen:
                 max_value = item.value
@@ -45,14 +45,41 @@ def solve_it(input_data):
         else:
             break
     taken = []
-    for i in enumerate(items):
+    for i in range(len(items)):
         if i in chosen:
             taken.append(1)
         else:
             taken.append(0)
     output_data_g1 = str(val) + ' ' + str(0) + '\n'
     output_data_g1 += ' '.join(map(str, taken))
-    # Greedy - Highest Value First
+    # Greedy - Highest Value First END
+    # Greedy - Smallest Weight First
+    chosen = []
+    tmp = items[0]
+    cap = capacity
+    val = 0
+    while 1:
+        min_weight = 1000000
+        for item in items:
+            if item.weight < min_weight and item.index not in chosen:
+                min_weight = item.weight
+                tmp = item
+
+        cap -= tmp.weight
+        if cap > 0:
+            val += tmp.value
+            chosen.append(tmp.index)
+        else:
+            break
+    taken = []
+    for i in range(len(items)):
+        if i in chosen:
+            taken.append(1)
+        else:
+            taken.append(0)
+    output_data_g2 = str(val) + ' ' + str(0) + '\n'
+    output_data_g2 += ' '.join(map(str, taken))
+    # Greedy - Smallest Weight First END
 
 
 
@@ -61,7 +88,7 @@ def solve_it(input_data):
     # prepare the solution in the specified output format
     # output_data = str(value) + ' ' + str(0) + '\n'
     # output_data += ' '.join(map(str, taken))
-    return output_data_g1
+    return output_data_g2
 
 
 if __name__ == '__main__':
