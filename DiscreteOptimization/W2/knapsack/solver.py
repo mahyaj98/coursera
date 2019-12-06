cd2d2153aa09d0a621b60b42aca9a693e87aa840
mahyaj98 <jamshidianm98@gmail.com>
2019-12-06 11:56:00 +0330

    Space Efficient DP - BUGGGG

--- DiscreteOptimization/W2/knapsack/solver.py
+++ DiscreteOptimization/W2/knapsack/solver.py
@@ -116,7 +116,7 @@ def solve_it(input_data):
         final_val = val
         final_output = output_data_g3
     # Greedy - Highest Density First END
-    # DP Space Effiient
+    # DP Space Efficient
     chosen = []
     K = [[0 for _ in range(capacity+1)] for _ in range(2)]
     i = 0
@@ -131,12 +131,12 @@ def solve_it(input_data):
 
         else:
             if items[i].weight <= j:
-                K[10][j] = max(K[1][j-items[i].weight], K[1][j])
+                K[10][j] = max(items[i].value + K[1][j-items[i].weight], K[1][j])
             else:
                 K[0][j] = K[1][j]
         if K[1][j] != K[0][j]:
             chosen.append(i)
-        i+=1
+        i += 1
 
     if len(items) % 2 == 0:
         val = K[0][capacity]
@@ -149,16 +149,15 @@ def solve_it(input_data):
             taken.append(1)
         else:
             taken.append(0)
-    output_data_dp = str(val) + ' ' + str(0) + '\n'
+    output_data_dp = str(val) + ' ' + str(1) + '\n'
     output_data_dp += ' '.join(map(str, taken))
     if val >= final_val:
         final_val = val
         final_output = output_data_dp
+    # DP Space Efficient END
 
 
 
-    
-    
     # prepare the solution in the specified output format
     # output_data = str(value) + ' ' + str(0) + '\n'
     # output_data += ' '.join(map(str, taken))
